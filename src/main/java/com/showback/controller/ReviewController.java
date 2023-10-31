b7f9c9abe46e959f46305b6f596ab58534174758
aass718 <sue05161@naver.com>
2023-11-01 08:51:17 +0900

    review service update

--- src/main/java/com/showback/controller/ReviewController.java
+++ src/main/java/com/showback/controller/ReviewController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/review")
 @RequiredArgsConstructor
@@ -18,12 +20,16 @@ public class ReviewController {
 
     @PostMapping
     public ResponseEntity<?> createReview(@RequestBody ReviewDTO reviewDTO,
+                                          HttpServletRequest request,
                                           BindingResult bindingResult) throws JsonProcessingException {
- if(bindingResult.hasErrors()){
+    if(bindingResult.hasErrors()){
             return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
-        Review review = reviewService.createReview(reviewDTO);
-        return ResponseEntity.ok().body(review);
+        String token = request.getHeader("Authorization").replace("Bearer ", "");
+//        Long userId = Long.parseLong(tokenProvider.validateAndGetUserId(token));
+//        Review review = reviewService.createReview(reviewDTO, userId);
+//        return ResponseEntity.ok().body(review);
+        return null;
     }
 
 }
--- src/main/java/com/showback/service/ReviewService.java
+++ src/main/java/com/showback/service/ReviewService.java
@@ -26,12 +26,9 @@ public class ReviewService {
     private  final ReviewMapper reviewMapper;
 
     @Transactional
-    public Review createReview(ReviewDTO reviewDTO) throws JsonProcessingException{
+    public Review createReview(ReviewDTO reviewDTO, Long userId) throws JsonProcessingException{
         UserAuth userAuth = null;
-        if(reviewDTO.getAuthId() != null){
-            userAuth = userAuthRepository.findById(reviewDTO.getAuthId())
-                    .orElseThrow(EntityNotFoundException::new);
-        }
+
         Review review = reviewMapper.toEntity(reviewDTO);
 
         return reviewRepository.save(review);
