7166db5107a814540d1e9f93f8aa32719dbc895a
o5ranc <o5ranc@github.com>
2023-11-06 17:59:22 +0900

    venue id 로 조회하기 추가

--- src/main/java/com/showback/controller/VenueController.java
+++ src/main/java/com/showback/controller/VenueController.java
@@ -1,15 +1,14 @@
 package com.showback.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.showback.dto.ShowDTO;
 import com.showback.dto.VenueDTO;
 import com.showback.model.Venue;
 import com.showback.repository.VenueRepository;
 import com.showback.service.VenueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/venue")
@@ -25,5 +24,12 @@ public class VenueController {
         Venue venue = venueService.createVenueWithSeats(venueDTO);
 
         return ResponseEntity.ok().body(venue);
+    }
+
+    @GetMapping("/{venueId}")
+    public ResponseEntity<VenueDTO> getVenue(@PathVariable("venueId") Long venueId)
+     throws JsonProcessingException {
+        VenueDTO venueDTO = venueService.findById(venueId);
+        return ResponseEntity.ok().body(venueDTO);
     }
 }
