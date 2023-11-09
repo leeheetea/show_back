e367be63b7188068be50cc6f3e70d2438142eae9
leeheetea <132877394+leeheetea@users.noreply.github.com>
2023-11-09 09:32:19 +0900

    1109

--- src/main/java/com/showback/controller/ReservationController.java
+++ src/main/java/com/showback/controller/ReservationController.java
@@ -1,36 +1,32 @@
 package com.showback.controller;
 
-import com.showback.dto.OrderDTO;
 import com.showback.dto.OrderRequestDTO;
+import com.showback.dto.ReservationDTO;
 import com.showback.model.Order;
-import com.showback.security.TokenProvider;
+import com.showback.model.Reservation;
 import com.showback.service.OrderService;
+import com.showback.service.ReservationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
-
-@RestController("/order")
+@RestController
+@RequestMapping("/reservation")
 @RequiredArgsConstructor
-public class OrderController {
+public class ReservationController {
 
+    private final ReservationService reservationService;
     private final OrderService orderService;
-    private final TokenProvider tokenProvider;
 
     @PostMapping
-    public ResponseEntity<?> createOrder(
+    public ResponseEntity<?> createReservation(
             @RequestBody OrderRequestDTO orderRequestDTO){
 
         Order order = orderService.createOrder(orderRequestDTO);
 
-        return ResponseEntity.ok().body(order);
+        return ResponseEntity.ok(order);
     }
 }
